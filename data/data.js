723efe6c063fe41de2f329dfa45741170fd2f3b0
Lflores2022 <launchx03687@innovaccion.mx>
2022-06-21 19:39:56 -0500

    subiendo quiz

---
+++ data/data.js
@@ -0,0 +1,132 @@
+export const data =[
+{
+    question: 'Es el lobulo que se encarga del razonamiento, movimiento, emociones y lenguaje',
+    choices: [
+        'temporal',
+        'frontal',
+        'occipital',
+        'paretial'
+    ],
+    answer: 'frontal'
+},
+{
+    question: 'Es el lobulo que se encarga del tacto, gusto, olfato, presion y temperatura',
+    choices: [
+        'temporal',
+        'frontal',
+        'occipital',
+        'paretial'
+    ],
+    answer: 'paretial'
+},
+{
+    question: 'Es el lobulo que se encarga de la vision',
+    choices: [
+        'temporal',
+        'frontal',
+        'occipital',
+        'paretial'
+    ],
+    answer: 'occipital'
+},
+{
+    question: 'Es el obulo encargado de la audicion, memoria y aprendizaje',
+    choices: [
+        'temporal',
+        'frontal',
+        'occipital',
+        'paretial'
+    ],
+    answer: 'temporal'
+},
+{
+    question: 'este neurotransmisor genera, te mantiene alerta y te brinda motivacion',
+    choices: [
+        'endorfina',
+        'dopamina',
+        'glutamato',
+        'adrenalina'
+    ],
+    answer: 'adrenalina'
+},
+{
+    question: 'este neurotransmisor te da inhibición del dolor y te da placer',
+    choices: [
+        'endorfina',
+        'dopamina',
+        'glutamato',
+        'adrenalina'
+    ],
+    answer: 'endorfina'
+},
+{
+    question: 'este neurotransmisor se encarga del estado de animo, deseo sexual y te matiene alerta',
+    choices: [
+        'endorfina',
+        'noradrenalina',
+        'oxitocina',
+        'adrenalina'
+    ],
+    answer: 'noradrenalina'
+},
+{
+    question: 'este neurotransmisor te brinda memoria, aprendizaje y estimulación muscular',
+    choices: [
+        'adrenalina',
+        'oxitocina',
+        'glutamato',
+        'acelticolina'
+    ],
+    answer: 'acelticolina'
+},
+{
+    question: 'este neurotransmisor equilibra el control de emociones',
+    choices: [
+        'endorfina',
+        'dopamina',
+        'cocaina',
+        'adrenalina'
+    ],
+    answer: 'dopamina'
+},
+{
+    question: 'este neurotransmisor emite memoria de aprendizaje y razonamiento',
+    choices: [
+        'serotonina',
+        'gaba',
+        'glutamato',
+        'adrenalina'
+    ],
+    answer: 'glutamato'
+},
+{
+    question: 'este neurotransmisor crea una balance emocional, regula el apetito, deseo sexual, temperatura y sueño',
+    choices: [
+        'endorfina',
+        'noradrenalina',
+        'serotonina',
+        'adrenalina'
+    ],
+    answer: 'serotonina'
+},
+{
+    question: 'este neurotransmisor crea concentracion, sueño y ansiedad',
+    choices: [
+        'endorfina',
+        'gaba',
+        'oxitocina',
+        'glutamato'
+    ],
+    answer: 'gaba'
+},
+{
+    question: 'este neurotransmisor se relaciona con vínculos afectivos, intimidad, confianza y fidelidad',
+    choices: [
+        'endorfina',
+        'gaba',
+        'oxitocina',
+        'glutamato'
+    ],
+    answer: 'oxitocina'
+},
+]
